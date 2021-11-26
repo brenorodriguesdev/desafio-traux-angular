24c63581b2ff26277e10ad81ec1d9755e9cef8b5
“Breno <brenorodriguesdev@gmail.com>
2021-11-26 19:33:02 -0300

    feat: add update category service

---
+++ src/services/category/update-category.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http"
+import { Injectable } from "@angular/core"
+import { environment } from "src/environments/environment"
+import { CategoryModel } from "src/models/category/category"
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UpdateCategoryService {
+
+    constructor(private http: HttpClient) { }
+
+    deleteById(data: CategoryModel) {
+        const token = localStorage.getItem("token")
+        return this.http.put<CategoryModel[]>(environment.url + 'category', data, {
+            headers: {
+                authorization: 'Bearer ' + token
+            }
+        })
+    }
+
+}
